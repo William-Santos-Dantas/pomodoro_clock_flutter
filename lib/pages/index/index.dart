e8ce3c70dd383c32c7b15e099ff1bdc2e2bc626b
unknown <NidrahDev@protonmail.com>
2021-03-07 18:51:11 -0300

    pomodoro clock without alarm

--- lib/pages/index/index.dart
+++ lib/pages/index/index.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:percent_indicator/percent_indicator.dart';
 
@@ -11,19 +10,24 @@ class IndexPage extends StatefulWidget {
 class _IndexPageState extends State<IndexPage> {
   var height;
   var width;
-  int minutes = 1;
+  int minutes = 25;
+  String text = "Minutos";
   String timer = "25:00";
   bool play = false;
   bool volume = true;
   int totalSeconds;
   int displayMinutes;
   String displaySeconds;
+  int currentTime;
+  double progressbar = 0;
+  var interval;
 
   @override
   void initState() {
     super.initState();
     totalSeconds = minutes * 60;
     displayMinutes = totalSeconds ~/ 60;
+    currentTime = totalSeconds;
     int seconds = totalSeconds % 60;
     if (seconds < 10) {
       displaySeconds = ("0$seconds");
@@ -33,19 +37,73 @@ class _IndexPageState extends State<IndexPage> {
   }
 
   void playTimer() {
-    int totalSecondsPlay = totalSeconds;
-    Timer.periodic(new Duration(seconds: 1), (timer) {
-      //if(totalSeconds == 0) timer.cancel();
+    interval = Timer.periodic(new Duration(seconds: 1), (timer) {
       setState(() {
-        totalSecondsPlay -= 1;
-        displayMinutes = totalSecondsPlay ~/ 60;
-        int seconds = totalSecondsPlay % 60;
+        progressbar += 1 / totalSeconds;
+        if (progressbar > 1) progressbar = 1;
+        currentTime -= 1;
+        displayMinutes = currentTime ~/ 60;
+        int seconds = currentTime % 60;
+        if (displayMinutes > 1) {
+          text = "Minutos";
+        } else if (displayMinutes == 1) {
+          text = "Minuto";
+        } else {
+          text = "Segundos";
+        }
         if (seconds < 10) {
-          displaySeconds = ("0$seconds");
+          displaySeconds = "0$seconds";
         } else {
           displaySeconds = seconds.toString();
         }
       });
+      if (currentTime == 0) {
+        progressbar = 1;
+        play = false;
+        timer.cancel();
+      }
+    });
+  }
+
+  void less() {
+    interval.cancel();
+    minutes -= 1;
+    totalSeconds = minutes * 60;
+    setState(() {
+      currentTime = totalSeconds;
+      progressbar = 0;
+      displayMinutes = currentTime ~/ 60;
+      displaySeconds = "0${currentTime % 60}";
+      text = "Minutos";
+    });
+  }
+
+  void stop() {
+    interval.cancel();
+  }
+
+  void more() {
+    interval.cancel();
+    minutes += 1;
+    totalSeconds = minutes * 60;
+    setState(() {
+      currentTime = totalSeconds;
+      progressbar = 0;
+      displayMinutes = currentTime ~/ 60;
+      displaySeconds = "0${currentTime % 60}";
+      text = "Minutos";
+    });
+  }
+
+  void reset() {
+    interval.cancel();
+    totalSeconds = minutes * 60;
+    setState(() {
+      currentTime = totalSeconds;
+      progressbar = 0;
+      displayMinutes = currentTime ~/ 60;
+      displaySeconds = "0${currentTime % 60}";
+      text = "Minutos";
     });
   }
 
@@ -88,7 +146,7 @@ class _IndexPageState extends State<IndexPage> {
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            icons(Icons.add),
+            icons(Icons.add, more),
             Container(
               height: 56,
               width: 56,
@@ -103,13 +161,13 @@ class _IndexPageState extends State<IndexPage> {
                 onPressed: () {
                   setState(() {
                     play = !play;
-                    if(play) playTimer();
+                    play ? playTimer() : stop();
                   });
                 },
               ),
             ),
-            icons(Icons.replay),
-            icons(Icons.remove),
+            icons(Icons.replay, reset),
+            icons(Icons.remove, less),
           ],
         )
       ],
@@ -119,12 +177,11 @@ class _IndexPageState extends State<IndexPage> {
   Widget progressIndicator() {
     return CircularPercentIndicator(
       radius: height * 0.35,
-      animation: true,
-      animationDuration: 1200,
+      animation: false,
       lineWidth: 15.0,
-      percent: 0.5,
+      percent: progressbar,
       center: new Text(
-        "$displayMinutes:$displaySeconds Minutes",
+        "$displayMinutes:$displaySeconds $text",
         style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
       ),
       circularStrokeCap: CircularStrokeCap.butt,
@@ -133,7 +190,7 @@ class _IndexPageState extends State<IndexPage> {
     );
   }
 
-  Widget icons(IconData icon) {
+  Widget icons(IconData icon, Function function) {
     return Container(
       height: 56,
       width: 56,
@@ -143,12 +200,7 @@ class _IndexPageState extends State<IndexPage> {
         shape: CircleBorder(),
       ),
       child: IconButton(
-        icon: Icon(icon),
-        color: Colors.white,
-        onPressed: () {
-          // TODO
-        },
-      ),
+          icon: Icon(icon), color: Colors.white, onPressed: function),
     );
   }
 }
